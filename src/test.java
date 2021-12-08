affc30cbe0f5c89ae92883bd353cc085f6d233c3
Mohammed M.Saleh <mhmd.anw22@gmail.com>
2021-12-08 23:18:22 +0300

    update test.java

--- src/test.java
+++ src/test.java
@@ -2,7 +2,7 @@ import java.math.BigInteger;
 
 public class test {
     public static void main(String[] args) {
-        BigInteger inverse = new BigInteger("13").modInverse(new BigInteger((""));
+        BigInteger inverse = new BigInteger("57").modInverse(new BigInteger("7972"));
         System.out.println(inverse);
     }
 }
